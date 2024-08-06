38fcaa59961aaebbfaa408d76b0f73c3bf47e426
Olesya Morozova <ovmorozova12@yandex.ru>
2024-08-06 23:52:26 +0400

    New change junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,7 +11,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
-        int actual = service.remain (amount);
+        int actual = service.remain(amount);
         int expected = 100;
 
         assertEquals(actual, expected);
@@ -22,7 +22,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 2200;
 
-        int actual = service.remain (amount);
+        int actual = service.remain(amount);
         int expected = 800;
 
         assertEquals(actual, expected);
@@ -33,7 +33,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
-        int actual = service.remain (amount);
+        int actual = service.remain(amount);
         int expected = 0;
 
         assertEquals(actual, expected);
